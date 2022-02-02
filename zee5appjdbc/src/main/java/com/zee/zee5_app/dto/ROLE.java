634a8a6c14756355ec4eaf26ac4e9d7c72f6b184
saurav.prasad <saurav.prasad@ZL-MU-MT-LT2161.zee.esselgroup.local>
2022-02-03 00:34:16 +0530

    .

--- zee5appjdbc/src/main/java/com/zee/zee5_app/dto/ROLE.java
+++
@@ -1,8 +0,0 @@
-package com.zee.zee5_app.dto;
-
-public enum ROLE {
-	
-	ROLE_USER,
-	ROLE_ADMIN
-
-}
--- zee5appspringboot/src/main/java/com/zee/zee5app/dto/ROLE.java
+++
@@ -1,8 +0,0 @@
-package com.zee.zee5app.dto;
-
-public enum ROLE {
-	
-	ROLE_USER,
-	ROLE_ADMIN
-
-}
