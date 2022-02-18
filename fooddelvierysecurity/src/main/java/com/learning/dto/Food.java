7c7607d9249ef4a58104230973e074b4b36219ce
Saurav Prasad <79800720+sauravpd29@users.noreply.github.com>
2022-02-18 20:03:54 +0530

    added prefix Auto Generate Id

--- fooddelvierysecurity/src/main/java/com/learning/dto/Food.java
+++ fooddelvierysecurity/src/main/java/com/learning/dto/Food.java
@@ -2,13 +2,19 @@ package com.learning.dto;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 import com.learning.dto.Food;
+import com.learning.utils.StringPrefixedSequenceIdGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -29,8 +35,14 @@ public class Food implements Comparable<Food> {
 
 	@Id
 	@Column(name = "id")
-	// @Length(min = 6)
-
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "my_seq")
+    @GenericGenerator(
+        name = "my_seq", 
+        strategy = "com.learning.utils.StringPrefixedSequenceIdGenerator", 
+        parameters = {
+            @Parameter(name = StringPrefixedSequenceIdGenerator.VALUE_PREFIX_PARAMETER, value = "Food_"),
+            @Parameter(name = StringPrefixedSequenceIdGenerator.NUMBER_FORMAT_PARAMETER, value = "%02d") })
+	
 	private String id;
 
 	@NotNull
